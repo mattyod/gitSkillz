ad50ff977ef53bf7192543168e44f015f474f773
mattyod <mattyod@gmail.com>
2018-08-22 14:24:26 +0100

    foo

--- foo.js
+++ foo.js
@@ -1,20 +1,2 @@
-function () {
-  var a = 1;
-  var b = 2;
-  console.log('asd');
-}
-
-function () {
-  var a = 1;
-  var b = 2;
-  console.log('asd');
-  var foo = {
-    asd: 1,
-    ase: 2
-  };
-}
-
-console.log('doo');
-
 foo
 foo
